0f6bb47e9ffdb8e5220e489578141964ade95db2
Tihonov Artyom <pop1212.1212@yandex.ru>
2024-12-09 21:25:27 +0300

    docker

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,14 @@ export default defineConfig({
   server: {
   proxy:{
   "/api":{
-  target: "http://localhost:8080",
+  target: "http://backend:8080",
   changeOrigin: true,
   }
-  }
-  }
+  },
+  watch: {
+    usePolling: true,
+  },
+  host: true,
+  strictPort: true,
+  },
 })
